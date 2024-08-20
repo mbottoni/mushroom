eb40a99441af393e519035d68ccca61749d45461
maruan <maruanbakriottoni@gmail.com>
2024-08-20 17:47:01 -0300

    jdklsa

---
+++ README.md
@@ -0,0 +1,8 @@
+# mushroom
+
+Mushroom kaggle competition
+
+
+```
+kaggle competitions submit -c playground-series-s4e8 -f prediction.csv -m "message"
+```
---
+++ src/submission.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+
+kaggle competitions submit -c playground-series-s4e8 -f ../data/submission_ef59.csv \
+                            -m "another submission message"
